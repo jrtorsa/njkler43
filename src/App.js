52a252c70509419a956f85bcc6d74d53ff266c55
Raul Torres <jr.torsa@gmail.com>
2021-07-15 23:01:25 -0600

    resolviendo el ejercicio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import posts from './posts'
-
+import React, { Component } from "react";
+import posts from "./posts";
 
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  state = {
+    search: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      search: e.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input
+            value={this.state.search}
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Ingresa el término de búsqueda"
+          />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {posts.filter( val => {
+            if(this.state.search === ''){
+              return val
+            } else if(val.title.toLocaleLowerCase().includes(this.state.search.toLocaleLowerCase())){
+              return val
+            }
+          }).map((post, index) => (
+            <li key={index}>
+              <a href={post.url}>
+                <img src={post.image} />
+              </a>
+              <p>{post.title}</p>
+            </li>
+          ))}
         </ul>
       </div>
-    )
+    );
   }
 }
 
-
-export default App
-
-
+export default App;
